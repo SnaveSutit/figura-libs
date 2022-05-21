17e301f8f7540461fbaa4c529126258f8271fe80
SnaveSutit <snavesutit@gmail.com>
2022-05-21 13:24:09 -0400

    Create healthEvents.lua

---
+++ healthEvents.lua
@@ -0,0 +1,38 @@
+local util = require("libs/util")
+
+local healthTracker =
+	util.newClass(
+	{
+		--- A simple health event listener
+		--- @param args {entity: LivingEntity}
+		constructor = function(self, args)
+			self.entity = args.entity
+			self.onDamage = args.onDamage
+			self.onHeal = args.onHeal
+			self.onChange = args.onChange
+
+			self.lastHealth = self.entity:getHealth()
+			events.TICK:register(
+				function()
+					self:tick()
+				end
+			)
+		end,
+		tick = function(self)
+			local health = self.entity:getHealth()
+			local delta = health - self.lastHealth
+
+			if (self.lastHealth > health) then
+				if (self.onChange) then self.onChange(delta) end
+				if (self.onDamage) then self.onDamage(delta) end
+			elseif (self.lastHealth < health) then
+				if (self.onChange) then self.onChange(delta) end
+				if (self.onHeal) then self.onHeal(delta) end
+			end
+
+			self.lastHealth = health
+		end
+	}
+)
+
+return healthTracker
